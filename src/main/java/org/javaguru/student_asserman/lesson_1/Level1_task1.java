d724705679700c2e55d4677f050f75af68f920f0
Sergey <448892@gmail.com>
2026-05-23 23:46:15 +0300

    lesson6

---
+++ src/main/java/org/javaguru/student_asserman/lesson_1/Level1_task1.java
@@ -0,0 +1,8 @@
+package org.javaguru.student_asserman.lesson_1;
+
+public class Level1_task1 {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+}
